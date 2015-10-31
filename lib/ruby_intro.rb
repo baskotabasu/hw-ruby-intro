fe6ec6c7c2cf94e47b11630021880a0a4a997168
baskotabasu <ruby.baskota@gmail.com>
2015-10-31 04:21:44 +0000

    Finished Home work assingment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,49 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
-end
+ def sum(array)
+  array.inject(0, :+)
+ end
 
-def max_2_sum arr
-  # YOUR CODE HERE
-end
+ def max_2_sum(array)
+   sum(array.sort.last(2))
+ end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
-
+ def sum_to_n? arr, n
+  arr.size.zero? ? false : arr.combination(2).any? {|i| i.reduce(:+) == n} 
+ end
+ 
 # Part 2
 
-def hello(name)
-  # YOUR CODE HERE
-end
+ def hello(name)
+  "Hello, #{name}"
+ end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
-end
+ def starts_with_consonant? s
+  alphabet = ('a'..'z').to_a - ['a', 'e', 'i', 'o', 'u']
+  s.empty? ? false : alphabet.member?(s[0].downcase)
+ end
+ 
+ def binary_multiple_of_4?(s)
+       int = s.to_i(2)
+       matching = /[^0-1]/.match(s)
+       (int % 4 == 0 and matching == nil and !s.empty?) ? true : false
+ end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
-end
 
 # Part 3
 
+
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? or price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    '$%0.2f' % @price
+  end
 end
